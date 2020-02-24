fcb2082a0dd37e68f28a44973f08f26a27c095e2
trustworthy-tutorial-7643 <sedlacek1991@gmail.com>
2020-02-24 00:53:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function add(x,y);
-function subtract (x,y);
-function multiply(x,y);
-divide ();
+function add(x,y) {
+  return (x +y)
+}
+function subtract (x,y) {
+  return (x -y)
+}
+function multiply(x,y) {
+  return (x *y)
+}
+function divide (x,y) {
+  return (x/y)
+}
 
 var number = 10
  
